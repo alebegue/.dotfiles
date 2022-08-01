db4d655ef593914b9392ef662ba65acdd5a230c0
Adrien Lebègue <adrien.lebegue@gmail.com>
2022-08-01 21:36:42 +0200

    Change for transparent background

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,4 +1,6 @@
 vim.g.tokyonight_style = "night"
+vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_transparent = true
 vim.g.tokyonight_dark_float = false
 vim.g.tokyonight_italic_comments = false
 vim.g.tokyonight_italic_keywords = false
