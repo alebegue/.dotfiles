403506e6d05fe726933dc86d1cd383ca61266933
Adrien Lebègue <adrien.lebegue@gmail.com>
2022-08-28 09:14:12 +0200

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,7 @@ set fish_color_valid_path
 
 # replace vim with neovim
 alias vim="nvim"
+alias vi="nvim"
 
 # replace ls with exa
 alias ls="exa -al --color=always --group-directories-first"
@@ -22,6 +23,29 @@ alias rm="rm -i"
 # adding flags
 alias df="df -h"
 alias free="free -m"
+
+# function to change color scheme
+function set_color_scheme;
+    set theme $argv[1]
+    sed -i "s/colors: \*.[A\-z].*/colors: \*$theme/g" /mnt/c/Users/AdrienLebegue/Documents/repos/dotfiles/alacritty/alacritty.yml &&
+
+    if [ -n $TMUX ]; tmux source-file ~/.tmux_$theme.conf;
+    end
+
+    if [ "$theme" = "light" ]; sed -i 's/vim.g.tokyonight_style = .*/vim.g.tokyonight_style = "day"/g' ~/repos/dotfiles/nvim/after/plugin/colors.lua;
+    else; sed -i 's/vim.g.tokyonight_style = .*/vim.g.tokyonight_style = "night"/g' ~/repos/dotfiles/nvim/after/plugin/colors.lua;
+    end
+end
+
+# set THEME environment variable
+#export THEME="dark"
+set theme $(sed -n "s/colors: \*\([A\-z]*\).*/\1/p" /mnt/c/Users/AdrienLebegue/Documents/repos/dotfiles/alacritty/alacritty.yml)
+#export THEME="$(cat /mnt/c/Users/AdrienLebegue/Documents/repos/dotfiles/alacritty/alacritty.yml | grep -oP '(?<=colors:\ \*).*[A-z]')"
+set_color_scheme $theme
+
+# switch between light and dark themes 
+alias ol="set_color_scheme light;"
+alias od="set_color_scheme dark;"
 
 if status is-interactive
     set -Ux PYENV_ROOT $HOME/.pyenv
