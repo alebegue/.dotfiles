497bb11f3348442d78289eccb1f7bbc930fdeeb1
Adrien Lebègue <adrien.lebegue@gmail.com>
2022-11-23 20:36:34 +0100

    remove telescope browser extension

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -7,7 +7,7 @@ local function telescope_buffer_dir()
     return vim.fn.expand('%:p:h')
 end
 
-local fb_actions = require("telescope").extensions.file_browser.actions
+-- local fb_actions = require("telescope").extensions.file_browser.actions
 
 telescope.setup({
     defaults = {
@@ -25,30 +25,30 @@ telescope.setup({
             end
         },
     },
-    extensions = {
-        file_browser = {
-            theme = "dropdown",
-            -- disables netrw and use telescope-file-browser in its place
-            hijack_netrw = true,
-            mappings = {
-                -- your custom insert mode mappings
-                ["i"] = {
-                    ["<C-w>"] = function() vim.cmd('normal vbd') end,
-                },
-                ["n"] = {
-                    -- your custom normal mode mappings
-                    ["N"] = fb_actions.create,
-                    ["h"] = fb_actions.goto_parent_dir,
-                    ["/"] = function()
-                        vim.cmd('startinsert')
-                    end
-                },
-            },
-        },
-    },
+    -- extensions = {
+    --     file_browser = {
+    --         theme = "dropdown",
+    --         -- disables netrw and use telescope-file-browser in its place
+    --         hijack_netrw = true,
+    --         mappings = {
+    --             -- your custom insert mode mappings
+    --             ["i"] = {
+    --                 ["<C-w>"] = function() vim.cmd('normal vbd') end,
+    --             },
+    --             ["n"] = {
+    --                 -- your custom normal mode mappings
+    --                 ["N"] = fb_actions.create,
+    --                 ["h"] = fb_actions.goto_parent_dir,
+    --                 ["/"] = function()
+    --                     vim.cmd('startinsert')
+    --                 end
+    --             },
+    --         },
+    --     },
+    -- },
 })
 
-telescope.load_extension("file_browser")
+-- telescope.load_extension("file_browser")
 
 -- vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
 -- vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
@@ -78,17 +78,17 @@ end)
 vim.keymap.set('n', ';e', function()
     builtin.diagnostics()
 end)
-vim.keymap.set("n", "<leader>sf",
-    function()
-        telescope.extensions.file_browser.file_browser({
-            path = "%:p:h",
-            cwd = telescope_buffer_dir(),
-            respect_gitignore = false,
-            hidden = true,
-            grouped = true,
-            previewer = false,
-            initial_mode = "normal",
-            layout_config = { height = 40 }
-        })
-    end
-)
+-- vim.keymap.set("n", "<leader>sf",
+--     function()
+--         telescope.extensions.file_browser.file_browser({
+--             path = "%:p:h",
+--             cwd = telescope_buffer_dir(),
+--             respect_gitignore = false,
+--             hidden = true,
+--             grouped = true,
+--             previewer = false,
+--             initial_mode = "normal",
+--             layout_config = { height = 40 }
+--         })
+--     end
+-- )
