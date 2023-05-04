8a5f67737f8e10bcf07f1558a0d252fc78098aa0
Adrien Lebègue <adrien.lebegue@gmail.com>
2023-05-04 21:24:01 +0200

    Add remap for buffers navigation

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -37,3 +37,5 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set("n", "<leader>b", "<cmd>ls<CR>:b<Space>", { silent = true })
