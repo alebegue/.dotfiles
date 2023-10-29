63f40bd2b2c3e977bd66bac4f549989de19d5e40
Adrien Lebègue <adrien.lebegue@gmail.com>
2023-10-29 14:36:32 +0100

    [IMP] fish: clean

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,14 +12,10 @@ alias vim="nvim"
 alias vi="nvim"
 
 # replace ls with exa
-alias l="exa -l --icons --git -a --no-user"
-alias ll="exa -l --icons --git -a"
-alias lt="exa --tree --level=2 --long --icons --git --no-user"
-alias llt="exa --tree --level=2 --long --icons --git"
-
-# confirm before overwriting something
-alias cp="cp -i"
-alias mv="mv -i"
+alias l="eza -l --icons --git -a --no-user"
+alias ll="eza -l --icons --git -a"
+alias lt="eza --tree --level=2 --long --icons --git --no-user"
+alias llt="eza --tree --level=2 --long --icons --git"
 
 # adding flags
 alias df="df -h"
@@ -31,12 +27,6 @@ bind -M insert \cf tmux-sessionizer
 # switch between light and dark color schemes
 alias ol="set-color-scheme light"
 alias od="set-color-scheme dark"
-
-set -Ux MKL_INTERFACE_LAYER GNU,LP64
-set -Ux MKL_THREADING_LAYER GNU
-
-if status is-interactive
-end
 
 # Fish git prompt
 set __fish_git_prompt_showuntrackedfiles yes
@@ -59,21 +49,8 @@ function fish_prompt
     set_color normal
 end
 
-function start_conda
-    #>>> conda initialize >>>
-    # !! Contents within this block are managed by 'conda init' !!
-    eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-    # <<< conda initialize <<<
-end
-# for CUDA: to run after env loaded
-# set -gx LD_LIBRARY_PATH $CONDA_PREFIX/lib/ $LD_LIBRARY_PATH
+# Starship
+starship init fish | source
 
-set -Ux JAVA_HOME /usr/lib/jvm/jdk-17
-set -Ux fish_user_paths $JAVA_HOME/bin $fish_user_paths
-
-source ~/.asdf/asdf.fish
+# pyenv
 pyenv init - | source
-if test -e "$VIRTUAL_ENV"; and test -f "$VIRTUAL_ENV/bin/activate.fish"
-    source "$VIRTUAL_ENV/bin/activate.fish"
-end
-# starship init fish | source
