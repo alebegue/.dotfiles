92b57903de79d25906d6a1a5ceee0c35e4bff26e
Adrien Lebègue <adrien.lebegue@gmail.com>
2024-12-08 23:44:07 +0100

    update telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
     },
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
+        branch = "0.1.x",
         dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
         config = function()
             local telescope = require("telescope")
