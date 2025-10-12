59400a55adb826fdbfede619d2f08116b68253eb
Adrien Lebègue <adrien.lebegue@gmail.com>
2025-10-12 22:02:55 +0200

    update nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,20 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+require("config.lazy")
 require("config.set")
 require("config.remap")
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins", {})
--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -91,17 +91,4 @@ return {
         "microsoft/python-type-stubs",
         ft = "python"
     },
-    {
-        "jay-babu/mason-null-ls.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            "williamboman/mason.nvim",
-            "jose-elias-alvarez/null-ls.nvim",
-        },
-        config = function()
-            require("mason-null-ls").setup({
-                automatic_setup = true,
-            })
-        end,
-    },
 }
