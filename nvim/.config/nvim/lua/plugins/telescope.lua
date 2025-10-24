19dd97bf64a5472daae1bdbe210e07e2a59b1741
Adrien Lebègue <adrien.lebegue@gmail.com>
2025-10-24 23:07:36 +0200

    fix telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     {
         "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make"
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release"
     },
     {
         "nvim-telescope/telescope.nvim",
